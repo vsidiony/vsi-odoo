fa32805e1978cc30d3bc7e3830bb978609331b67
vsidiony <diony.castro@vettersystems.com>
2021-11-03 09:35:05 +0800

    Add remarks field

--- custom/dsc_customsales/models/sale.py
+++ custom/dsc_customsales/models/sale.py
@@ -5,3 +5,4 @@ class DscStormLog(models.Model):
     _inherit = "sale.order"
 
     storm_sale_description = fields.Char(string="Storm Sales")
+    storm_sale_remarks = fields.Char(string="Storm Sales Remarks")
